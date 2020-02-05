ca171afdabc0056ee6a797bdf18617e9bec37e43
sega surfs linux <piuskri@gmail.com>
2020-02-05 11:58:12 +0100

    add assmble timer output

---
+++ DolfinFisherSolver/build/CMakeFiles/FisherSolver.dir/DependInfo.cmake
@@ -0,0 +1,54 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pius/Projects/DolfinFisherSolver/DolfinFisherSolver/Initializers.cpp" "/home/pius/Projects/DolfinFisherSolver/DolfinFisherSolver/build/CMakeFiles/FisherSolver.dir/Initializers.cpp.o"
+  "/home/pius/Projects/DolfinFisherSolver/DolfinFisherSolver/ReactionDiffusionProblem.cpp" "/home/pius/Projects/DolfinFisherSolver/DolfinFisherSolver/build/CMakeFiles/FisherSolver.dir/ReactionDiffusionProblem.cpp.o"
+  "/home/pius/Projects/DolfinFisherSolver/DolfinFisherSolver/ReaderWriter.cpp" "/home/pius/Projects/DolfinFisherSolver/DolfinFisherSolver/build/CMakeFiles/FisherSolver.dir/ReaderWriter.cpp.o"
+  "/home/pius/Projects/DolfinFisherSolver/DolfinFisherSolver/Tensors.cpp" "/home/pius/Projects/DolfinFisherSolver/DolfinFisherSolver/build/CMakeFiles/FisherSolver.dir/Tensors.cpp.o"
+  "/home/pius/Projects/DolfinFisherSolver/DolfinFisherSolver/TimeStepper.cpp" "/home/pius/Projects/DolfinFisherSolver/DolfinFisherSolver/build/CMakeFiles/FisherSolver.dir/TimeStepper.cpp.o"
+  "/home/pius/Projects/DolfinFisherSolver/DolfinFisherSolver/main.cpp" "/home/pius/Projects/DolfinFisherSolver/DolfinFisherSolver/build/CMakeFiles/FisherSolver.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DOLFIN_VERSION=\"2019.1.0\""
+  "HAS_BOOST"
+  "HAS_CHOLMOD"
+  "HAS_HDF5"
+  "HAS_MPI"
+  "HAS_PETSC"
+  "HAS_SCOTCH"
+  "HAS_SLEPC"
+  "HAS_UMFPACK"
+  "HAS_ZLIB"
+  "NDEBUG"
+  "_FORTIFY_SOURCE=2"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/eigen3"
+  "/usr/include/hdf5/openmpi"
+  "/usr/lib/x86_64-linux-gnu/openmpi/include/openmpi"
+  "/usr/lib/x86_64-linux-gnu/openmpi/include/openmpi/opal/mca/event/libevent2022/libevent"
+  "/usr/lib/x86_64-linux-gnu/openmpi/include/openmpi/opal/mca/event/libevent2022/libevent/include"
+  "/usr/lib/x86_64-linux-gnu/openmpi/include"
+  "/usr/lib/slepcdir/3.7.4/x86_64-linux-gnu-real/include"
+  "/usr/lib/petscdir/3.7.7/x86_64-linux-gnu-real/include"
+  "/usr/include/hypre"
+  "/usr/include/superlu"
+  "/usr/include/suitesparse"
+  "/usr/lib/x86_64-linux-gnu/hdf5/openmpi/include"
+  "/usr/include/scotch"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
