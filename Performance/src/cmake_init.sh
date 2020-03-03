c1b9484924b434151423e9864d8f2cb1e38e84b1
sega surfs linux <piuskri@gmail.com>
2020-03-03 17:37:11 +0100

    add cmake init script

---
+++ Performance/src/cmake_init.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# source $HOME/usr/source_env.sh first before running this
+
+rm -rf build
+mkdir -p build
+cd build && \
+    cmake -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpic++ -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
