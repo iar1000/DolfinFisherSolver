2e0c0043e51fcd4b2b47f2378df94d6470136dd7
sega surfs linux <piuskri@gmail.com>
2020-03-13 20:01:07 +0100

    ok

--- Performance/src/runPerformanceTest.sh
+++ Performance/src/runPerformanceTest.sh
@@ -15,6 +15,7 @@ while [[ "$#" -gt 0 ]]; do case $1 in
   -me|--memory) MEM="$2"; shift;;
   -tol|--tolerance) TOL="$2"; shift;;
   -t|--type) TYPE="$2"; shift;;
+  -knz| --nonzero) KRYLNONZERO="$2"; shift;;
  
   *) echo "Unknown parameter passed: $1"; exit 1;;
 esac; shift; done
