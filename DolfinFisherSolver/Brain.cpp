e5b68c0084f69cc23a5549a4249254ee257e26ce
sega surfs linux <piuskri@gmail.com>
2020-04-07 19:52:30 +0200

    functions to find best linear transformation for fitting mesh matrices on brainweb matrices

--- DolfinFisherSolver/Brain.cpp
+++ DolfinFisherSolver/Brain.cpp
@@ -13,13 +13,19 @@ Brain::Brain(int rank, int verbose, std::string dataParentPath){
 	verbose_ = verbose;
 	dataParentPath_ = dataParentPath;
 	// initialize extremas and read in data
-	for(int i = 0; i < 3; i++){	max_cm_[i] = -99999; min_cm_[i] = 999999;	}
-	readData();
+	middle_x = 88;
+	for(int i = 0; i < 3; i++){
+		max_cm_[i] = -99999;
+		min_cm_[i] = 999999;
+		max_mesh_[i] = -99999;
+		min_mesh_[i] = 999999;
+	}
+	readBrainweb();
 }
 
 
 
-void Brain::readData(){
+void Brain::readBrainweb(){
 	if(rank_ == 0 && verbose_ > 3){ std::cout << "	read in BrainWeb tissue data..." << std::endl; }
 	std::string greyPath = dataParentPath_ + "/phantom_1.0mm_normal_gry.rawb";
 	std::string whitePath = dataParentPath_ + "/phantom_1.0mm_normal_wht.rawb";
@@ -80,76 +86,201 @@ void Brain::readData(){
 				"		y: [" << min_cm_[1] << ", " << max_cm_[1] << "]" << " (" << lengths_[1] << ")" << std::endl <<
 				"		z: [" << min_cm_[2] << ", " << max_cm_[2] << "]" << " (" << lengths_[2] << ")" << std::endl;
 	}
+}
 
-	// create files to convert with python script to picture/ gif
-	/*
-	if(verbose_ > 3){
-		std::cout << "	output slice data to: " << dataParentPath_ << "/brain_slices_csv" << std::endl;
-		std::string ss = dataParentPath_ + "/brain_slices_csv";
-		mkdir(ss.c_str(), 0777);
-		for(int zz = 0; zz < z; zz++){
-			// output debug image
-			std::stringstream s;
-			s << dataParentPath_ << "/brain_slices_csv/white-z-" << zz << ".txt";
-			std::ofstream out(s.str(), std::ios::trunc);
-			for(int p = 0; p < y; p++){
-				for(int q = 0; q < x; q++){
-					out << (worker1.at(zz)(p, q) * 255) << ",";
-				}
-				out << "\n";
+void Brain::readMesh(std::shared_ptr<dolfin::Mesh> mesh){
+	// setup of variables
+	if(rank_ == 0 && verbose_ > 3){ std::cout << "	read in mesh data..." << std::endl; }
+	int x = dim_[0];
+	int y = dim_[1];
+	int z = dim_[2];
+	char *buf = new char[1];
+
+	// initialize local empty concentration map
+	mms_.clear();
+	for(int i = 0; i < 3; i++){
+		max_mesh_[i] = -99999;
+		min_mesh_[i] = 999999;
+	}
+	std::vector<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>> worker;
+	for(int k = 0; k < z; k++){
+		Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> slice = Eigen::MatrixXd::Zero(y, x);
+		worker.push_back(slice);
+	}
+
+	// go through coordinates
+	auto coordinates = mesh->coordinates();
+	for(int i = 0; i < coordinates.size(); i += 3){
+		int x[2] = {(int)floor(coordinates[i]), (int)ceil(coordinates[i])};
+		int y[2] = {(int)floor(coordinates[i+1]), (int)ceil(coordinates[i+1])};
+		int z[2] = {(int)floor(coordinates[i+2]), (int)ceil(coordinates[i+2])};
+
+		// set markers
+		for(int j = 0; j < 2; j++){
+			for(int k = 0; k < 2; k++){	// virtual mesh grid map
+				worker.at(z[j])(y[k], x[0]) = 1;
+				worker.at(z[j])(y[k], x[1]) = 1;
 			}
-			out.close();
 		}
-		for(int zz = 0; zz < z; zz++){
-			// output debug image
-			std::stringstream s;
-			s << dataParentPath_ << "/brain_slices_csv/grey-z-" << zz << ".txt";
-			std::ofstream out(s.str(), std::ios::trunc);
-			for(int p = 0; p < y; p++){
-				for(int q = 0; q < x; q++){
-					out << (worker2.at(zz)(p, q) * 255) << ",";
+
+		// update bounding box
+		if(coordinates[i] > max_mesh_[0]){ max_mesh_[0] = coordinates[i]; }
+		if(coordinates[i] < min_mesh_[0]){ min_mesh_[0] = coordinates[i]; }
+		if(coordinates[i+1] > max_mesh_[1]){ max_mesh_[1] = coordinates[i+1]; }
+		if(coordinates[i+1] < min_mesh_[1]){ min_mesh_[1] = coordinates[i+1]; }
+		if(coordinates[i+2] > max_mesh_[2]){ max_mesh_[2] = coordinates[i+2]; }
+		if(coordinates[i+2] < min_mesh_[2]){ min_mesh_[2] = coordinates[i+2]; }
+	}
+	mms_ = worker;
+
+	// extract important parameters
+	for(int i = 0; i < 3; i++){ lengths_mesh_[i] = max_mesh_[i] - min_mesh_[i]; }
+	// print dimensions
+	if(rank_ == 0 && verbose_ > 2){
+		std::cout << "	mesh bounds: " << std::endl <<
+				"		x: [" << min_mesh_[0] << ", " << max_mesh_[0] << "]" << " (" << lengths_mesh_[0] << ")" << std::endl <<
+				"		y: [" << min_mesh_[1] << ", " << max_mesh_[1] << "]" << " (" << lengths_mesh_[1] << ")" << std::endl <<
+				"		z: [" << min_mesh_[2] << ", " << max_mesh_[2] << "]" << " (" << lengths_mesh_[2] << ")" << std::endl;
+	}
+}
+
+std::vector<int> Brain::compareTranslated(std::shared_ptr<dolfin::Mesh> mesh, int translation[3], bool print){
+	if(rank_ == 0 && verbose_ > 3){
+		std::cout << "	compare brainweb to mesh fit, translation: " << translation[0] << ", " << translation[1] << ", " << translation[2] << std::endl;
+	}
+	// create empty comparison map
+	std::vector<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>> worker;
+	for(int k = 0; k < dim_[2]; k++){
+		Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> slice = Eigen::MatrixXd::Zero(dim_[1], dim_[0]);
+		worker.push_back(slice);
+	}
+	// counter variables
+	int hit_white = 0;
+	int hit_grey = 0;
+	int miss = 0;
+	int tries = 0;
+	// coordinates
+	auto coordinates = mesh->coordinates();
+	for(int i = 0; i < coordinates.size(); i += 3){
+		// get coordinates
+		int x[2] = {(int)floor(coordinates[i]), (int)ceil(coordinates[i])};
+		int y[2] = {(int)floor(coordinates[i+1]), (int)ceil(coordinates[i+1])};
+		int z[2] = {(int)floor(coordinates[i+2]), (int)ceil(coordinates[i+2])};
+		// translate
+		for(int t = 0; t < 2; t++){
+			x[t] += translation[0];
+			y[t] += translation[1];
+			z[t] += translation[2];
+		}
+		// set markers
+		for(int j = 0; j < 2; j++){
+			for(int k = 0; k < 2; k++){
+				tries += 2;
+				// if grey or white hits then mark the spot
+				if(cms_[0].at(z[j])(y[k], x[0]) != 0 ||
+						cms_[1].at(z[j])(y[k], x[0]) != 0){
+					worker.at(z[j])(y[k], x[0]) = 1;
+					// counter
+					if(cms_[0].at(z[j])(y[k], x[0]) != 0){ hit_white++; }
+					if(cms_[1].at(z[j])(y[k], x[0]) != 0){ hit_grey++; }
 				}
-				out << "\n";
+				else{ miss++; }
+				if(cms_[0].at(z[j])(y[k], x[1]) != 0 ||
+						cms_[1].at(z[j])(y[k], x[1]) != 0){
+					worker.at(z[j])(y[k], x[1]) = 1;
+					// counter
+					if(cms_[0].at(z[j])(y[k], x[1]) != 0){ hit_white++; }
+					if(cms_[1].at(z[j])(y[k], x[1]) != 0){ hit_grey++; }
+				}
+				else{ miss ++; }
 			}
-			out.close();
 		}
 	}
-	*/
+	// print
+	if(rank_ == 0 && verbose_ > 3){
+		std::cout << "		tries/white/grey/miss: " << tries << "/" << hit_white << "/" << hit_grey << "/" << miss << std::endl;
+	}
+	if(print){
+		std::stringstream ss;
+		ss << "fit-" << translation[0] << "-" << translation[1] << "-" << translation[2];
+		printVectorMatrix(worker, ss.str());
+	}
 
+	std::vector<int> r;
+	r.push_back(miss);
+	r.push_back(hit_white);
+	r.push_back(hit_grey);
+	r.push_back(tries);
+	return r;
+}
+
+void Brain::fitMesh(){
+	std::vector<std::string> paths;
+	paths.push_back("../brain-data/mesh/lh-plial-hull-flood-0-1-merge-4-dof-100k.xml");
+	paths.push_back("../brain-data/mesh/rh-plial-hull-flood-0-1-merge-4-dof-100k.xml");
+	paths.push_back("../brain-data/mesh/lh-white-hull-flood-0-1-merge-5-dof-100k.xml");
+	paths.push_back("../brain-data/mesh/rh-white-hull-flood-0-1-merge-5-dof-100k.xml");
+
+	int eps = 15;
+	for(int i = 0; i < paths.size(); i++){
+		std::shared_ptr<dolfin::Mesh> mesh = std::make_shared<dolfin::Mesh>(paths.at(i));
+		auto result = greedyOptimalTranslation(mesh, eps);
+		if(rank_ == 0 && verbose_ > 2){
+			std::cout << paths.at(i) << "\n	best translation in misses " << (int)((double)result.at(1) / (double)result.at(0) * 100) << "%, translation: " <<
+					result.at(2) << ", " << result.at(3) << ", " << result.at(4) << std::endl << std::endl;
+		}
+	}
 
 }
 
-void Brain::fitMesh(std::shared_ptr<dolfin::Mesh> mesh){
-	// variables
-	double max[3];
-	double min[3];
-	for(int i = 0; i < 3; i++){
-		max[i] = -99999;
-		min[i] = 99999;
-	}
-	// go through coordinates
-	int num_dim = 3;
-	auto coordinates = mesh->coordinates();
-	for(int i = 0; i < coordinates.size(); i += num_dim){
-		// update extremas
-		if(coordinates[i] > max[0]){ max[0] = coordinates[i]; }
-		if(coordinates[i] < min[0]){ min[0] = coordinates[i]; }
-		if(coordinates[i+1] > max[1]){ max[1] = coordinates[i+1]; }
-		if(coordinates[i+1] < min[1]){ min[1] = coordinates[i+1]; }
-		if(coordinates[i+2] > max[2]){ max[2] = coordinates[i+2]; }
-		if(coordinates[i+2] < min[2]){ min[2] = coordinates[i+2]; }
-	}
-	// extract important parameters
-	double lengths[3];
-	for(int i = 0; i < 3; i++){ lengths[i] = max[i] - min[i]; }
+std::vector<int> Brain::greedyOptimalTranslation(std::shared_ptr<dolfin::Mesh> mesh, int eps){
+	if(rank_ == 0 && verbose_ > 2){	std::cout << "find optimal translation from mesh to brainweb within eps= " << eps << "..." << std::endl; }
+	// update internal mesh
+	readMesh(mesh);
 
-	if(rank_ == 0 && verbose_ > 3){
-		std::cout << "	mesh bounds: " << std::endl <<
-				"		x: [" << min[0] << ", " << max[0] << "]" << " (" << lengths[0] << ")" << std::endl <<
-				"		y: [" << min[1] << ", " << max[1] << "]" << " (" << lengths[1] << ")" << std::endl <<
-				"		z: [" << min[2] << ", " << max[2] << "]" << " (" << lengths[2] << ")" << std::endl;
-	}
+	// rough alignment of mesh to brainweb datamap
+	int fittedTranslation[3];
+	fittedTranslation[0] = min_cm_[0] - (int)floor(min_mesh_[0]);
+	fittedTranslation[1] = min_cm_[1] - (int)floor(min_mesh_[1]);
+	fittedTranslation[2] = max_cm_[2] - (int)ceil(max_mesh_[2]);
+	if(rank_ == 0 && verbose_ > 2){ std::cout << "	bounding translation: " << fittedTranslation[0] << ", " << fittedTranslation[1] << ", " << fittedTranslation[2] << std::endl; }
 
+	// calculate bounds of translation
+	int x_start = (((int)floor(min_mesh_[0]) + fittedTranslation[0] - eps) > 0 ? (fittedTranslation[0] - eps) : (int)floor(min_mesh_[0]));
+	int y_start = (((int)floor(min_mesh_[1]) + fittedTranslation[1] - eps) > 0 ? (fittedTranslation[1] - eps) : (int)floor(min_mesh_[1]));
+	int z_start = (((int)floor(min_mesh_[2]) + fittedTranslation[2] - eps) > 0 ? (fittedTranslation[2] - eps) : (int)floor(min_mesh_[2]));
+	int x_end = (((int)ceil(max_mesh_[0]) + fittedTranslation[0] + eps) < dim_[0] ? (fittedTranslation[0] + eps) : (dim_[0] - (int)ceil(max_mesh_[0])));
+	int y_end = (((int)ceil(max_mesh_[1]) + fittedTranslation[1] + eps) < dim_[1] ? (fittedTranslation[1] + eps) : (dim_[1] - (int)ceil(max_mesh_[1])));
+	int z_end = (((int)ceil(max_mesh_[2]) + fittedTranslation[2] + eps) < dim_[2] ? (fittedTranslation[2] + eps) : (dim_[2] - (int)ceil(max_mesh_[2])));
+
+	// calculate best translation with respect to misses
+	std::vector<int> best;
+	best.push_back(99999999);			// 	total tries
+	best.push_back(99999999);			//	#misses
+	best.push_back(-1);					//  x
+	best.push_back(-1);					//  y
+	best.push_back(-1);					//  z
+	for(int x = x_start; x < x_end; x++){
+		for(int y = y_start; y < y_end; y++){
+			for(int z = z_start; z < z_end; z++){
+				int translation[3] = {x, y, z};
+				// get result
+				auto result = compareTranslated(mesh, translation, false);
+				// compare to best
+				if(result.at(0) < best.at(1)){
+					best.at(0) = result.at(3);
+					best.at(1) = result.at(0);
+					best.at(2) = x;
+					best.at(3) = y;
+					best.at(4) = z;
+					if(rank_ == 0 && verbose_ > 3){
+						std::cout << "		new best: " << result.at(0) << "  by translation " << x << " " << y << " " << z << std::endl;
+					}
+				}
+			}
+		}
+	}
+	return best;
 }
 
 std::pair<std::vector<std::vector<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>>>, int*>
@@ -157,4 +288,30 @@ std::pair<std::vector<std::vector<Eigen::Matrix<double, Eigen::Dynamic, Eigen::D
 	std::pair<std::vector<std::vector<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>>>, int*> p;
 	p = std::make_pair(cms_, dim_);
 	return p;
+}
+
+void Brain::printVectorMatrix(std::vector<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>> data, std::string name){
+	int x = dim_[0];
+	int y = dim_[1];
+	int z = dim_[2];
+	int plane = x*y;
+	// create files to convert with python script to picture/ gif
+	if(rank_ == 0 && verbose_ > 3){
+		std::cout << "	output slice data to: " << dataParentPath_ << "/" << name << std::endl;
+	}
+	std::string ss = dataParentPath_ + "/" + name;
+	mkdir(ss.c_str(), 0777);
+	for(int zz = 0; zz < z; zz++){
+		// output debug image
+		std::stringstream s;
+		s << dataParentPath_ << "/" << name << "/slice-" << zz << ".txt";
+		std::ofstream out(s.str(), std::ios::trunc);
+		for(int p = 0; p < y; p++){
+			for(int q = 0; q < x; q++){
+				out << (data.at(zz)(p, q) * 255) << ",";
+			}
+			out << "\n";
+		}
+		out.close();
+	}
 }
--- DolfinFisherSolver/Brain.h
+++ DolfinFisherSolver/Brain.h
@@ -10,22 +10,38 @@ class Brain{
 
 	int rank_;							// rank of caller
 	int verbose_;						// verbosity
-	// brainweb raw specific
 	std::string dataParentPath_;		// path to brainweb data folder
-	int dim_[3] = {181, 217, 181};		// dimensions of brainweb data
+	int dim_[3] = {181, 217, 181};		// dimensions of bounding box
+	// brainweb
 	std::vector<std::vector<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>>> cms_;	// virtual brainweb concentration data maps
-	// brainweb additional info
 	double max_cm_[3];					// maximum values of dimensions x,y,z
 	double min_cm_[3];					// minimum values of dimensions x,y,z
 	double lengths_[3];					// length of span between extremas
+	double middle_x;				// cutoff for left brainhalf
+	// mesh
+	std::vector<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>> mms_;	// virtual mesh grid map
+	double max_mesh_[3];					// maximum values of dimensions x,y,z
+	double min_mesh_[3];					// minimum values of dimensions x,y,z
+	double lengths_mesh_[3];				// length of span between extremas
+	double middle_x_mesh_;				// cutoff for left brainhalf
+
 	// read in binary data from brainweb
-	void readData();
+	void readBrainweb();
+	// read in mesh data
+	void readMesh(std::shared_ptr<dolfin::Mesh> mesh);
+	// find optimal translation within eps of the bounding traslation
+	// returns total tries, #misses, translations in x, y and z
+	std::vector<int> greedyOptimalTranslation(std::shared_ptr<dolfin::Mesh> mesh, int eps);
+	// compare virtual maps
+	std::vector<int> compareTranslated(std::shared_ptr<dolfin::Mesh> mesh, int translation[3], bool print);
+	// print out the virtual maps
+	void printVectorMatrix(std::vector<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>> data, std::string name);
 
 public:
 	// constructor for virtual brain
 	Brain(int rank, int verbose, std::string dataParentPath);
-	// fit the tissue concentration map on the mesh version 1 for 3D
-	void fitMesh(std::shared_ptr<dolfin::Mesh> mesh);
+	// fit the tissue concentration map on the different meshes and fit best one
+	void fitMesh();
 	// get concentration map and dimenstionality
 	std::pair<std::vector<std::vector<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>>>, int*>
 		getConcentrationMap();
