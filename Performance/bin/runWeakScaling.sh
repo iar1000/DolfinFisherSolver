5936dfd807b82bb423812a835280c61c2ebb9364
sega surfs linux <piuskri@gmail.com>
2020-04-08 21:18:37 +0200

    weak scaling 24h

--- Performance/bin/runWeakScaling.sh
+++ Performance/bin/runWeakScaling.sh
@@ -15,19 +15,19 @@ while [[ "$#" -gt 0 ]]; do case $1 in
 esac; shift; done
 
 # run weak scaling test on different mesh sizes
-bsub -n 2 mpirun ./Performance-FisherSolver --name "weakscaling-40k" --meshname "lh-white-hull-flood-0-1-merge-5-dof-80k.xml" --type "$TYPE" --newton_tol "$TOL" --krylovnonzero "$KRYLNONZERO"
-bsub -n 16 mpirun ./Performance-FisherSolver --name "weakscaling-40k" --meshname "lh-white-hull-flood-0-1-merge-5-dof-600k.xml" --type "$TYPE" --newton_tol "$TOL" --krylovnonzero "$KRYLNONZERO"
-bsub -n 120 -R fullnode mpirun ./Performance-FisherSolver --name "weakscaling-40k" --meshname "lh-white-hull-flood-0-1-merge-5-dof-4700k.xml" --type "$TYPE" --newton_tol "$TOL" --krylovnonzero "$KRYLNONZERO"
+bsub -W 24:00 -n 2 mpirun ./Performance-FisherSolver --name "weakscaling-40k" --meshname "lh-white-hull-flood-0-1-merge-5-dof-80k.xml" --type "$TYPE" --newton_tol "$TOL" --krylovnonzero "$KRYLNONZERO"
+bsub -W 24:00 -n 16 mpirun ./Performance-FisherSolver --name "weakscaling-40k" --meshname "lh-white-hull-flood-0-1-merge-5-dof-600k.xml" --type "$TYPE" --newton_tol "$TOL" --krylovnonzero "$KRYLNONZERO"
+bsub -W 24:00 -n 120 -R fullnode mpirun ./Performance-FisherSolver --name "weakscaling-40k" --meshname "lh-white-hull-flood-0-1-merge-5-dof-4700k.xml" --type "$TYPE" --newton_tol "$TOL" --krylovnonzero "$KRYLNONZERO"
 
-bsub -n 1 mpirun ./Performance-FisherSolver --name "weakscaling-80k" --meshname "lh-white-hull-flood-0-1-merge-5-dof-80k.xml" --type "$TYPE" --newton_tol "$TOL" --krylovnonzero "$KRYLNONZERO"
-bsub -n 8 mpirun ./Performance-FisherSolver --name "weakscaling-80k" --meshname "lh-white-hull-flood-0-1-merge-5-dof-600k.xml" --type "$TYPE" --newton_tol "$TOL" --krylovnonzero "$KRYLNONZERO"
-bsub -n 60 mpirun ./Performance-FisherSolver --name "weakscaling-80k" --meshname "lh-white-hull-flood-0-1-merge-5-dof-4700k.xml" --type "$TYPE" --newton_tol "$TOL" --krylovnonzero "$KRYLNONZERO"
-bsub -n 463 mpirun ./Performance-FisherSolver --name "weakscaling-80k" --meshname "lh-white-hull-flood-0-1-merge-5-dof-37000k.xml" --type "$TYPE" --newton_tol "$TOL" --krylovnonzero "$KRYLNONZERO"
+bsub -W 24:00 -n 1 mpirun ./Performance-FisherSolver --name "weakscaling-80k" --meshname "lh-white-hull-flood-0-1-merge-5-dof-80k.xml" --type "$TYPE" --newton_tol "$TOL" --krylovnonzero "$KRYLNONZERO"
+bsub -W 24:00 -n 8 mpirun ./Performance-FisherSolver --name "weakscaling-80k" --meshname "lh-white-hull-flood-0-1-merge-5-dof-600k.xml" --type "$TYPE" --newton_tol "$TOL" --krylovnonzero "$KRYLNONZERO"
+bsub -W 24:00 -n 60 mpirun ./Performance-FisherSolver --name "weakscaling-80k" --meshname "lh-white-hull-flood-0-1-merge-5-dof-4700k.xml" --type "$TYPE" --newton_tol "$TOL" --krylovnonzero "$KRYLNONZERO"
+bsub -W 24:00 -n 463 mpirun ./Performance-FisherSolver --name "weakscaling-80k" --meshname "lh-white-hull-flood-0-1-merge-5-dof-37000k.xml" --type "$TYPE" --newton_tol "$TOL" --krylovnonzero "$KRYLNONZERO"
 
-bsub -n 4 mpirun ./Performance-FisherSolver --name "weakscaling-150k" --meshname "lh-white-hull-flood-0-1-merge-5-dof-600k.xml" --type "$TYPE" --newton_tol "$TOL" --krylovnonzero "$KRYLNONZERO"
-bsub -n 30 mpirun ./Performance-FisherSolver --name "weakscaling-150k" --meshname "lh-white-hull-flood-0-1-merge-5-dof-4700k.xml" --type "$TYPE" --newton_tol "$TOL" --krylovnonzero "$KRYLNONZERO"
-bsub -n 240 -R fullnode mpirun ./Performance-FisherSolver --name "weakscaling-150k" --meshname "lh-white-hull-flood-0-1-merge-5-dof-37000k.xml" --type "$TYPE" --newton_tol "$TOL" --krylovnonzero "$KRYLNONZERO"
+bsub -W 24:00 -n 4 mpirun ./Performance-FisherSolver --name "weakscaling-150k" --meshname "lh-white-hull-flood-0-1-merge-5-dof-600k.xml" --type "$TYPE" --newton_tol "$TOL" --krylovnonzero "$KRYLNONZERO"
+bsub -W 24:00 -n 30 mpirun ./Performance-FisherSolver --name "weakscaling-150k" --meshname "lh-white-hull-flood-0-1-merge-5-dof-4700k.xml" --type "$TYPE" --newton_tol "$TOL" --krylovnonzero "$KRYLNONZERO"
+bsub -W 24:00 -n 240 -R fullnode mpirun ./Performance-FisherSolver --name "weakscaling-150k" --meshname "lh-white-hull-flood-0-1-merge-5-dof-37000k.xml" --type "$TYPE" --newton_tol "$TOL" --krylovnonzero "$KRYLNONZERO"
 
-bsub -n 1 mpirun ./Performance-FisherSolver --name "weakscaling-600k" --meshname "lh-white-hull-flood-0-1-merge-5-dof-600k.xml" --type "$TYPE" --newton_tol "$TOL" --krylovnonzero "$KRYLNONZERO"
-bsub -n 8 mpirun ./Performance-FisherSolver --name "weakscaling-600k" --meshname "lh-white-hull-flood-0-1-merge-5-dof-4700k.xml" --type "$TYPE" --newton_tol "$TOL" --krylovnonzero "$KRYLNONZERO"
-bsub -n 60 mpirun ./Performance-FisherSolver --name "weakscaling-600k" --meshname "lh-white-hull-flood-0-1-merge-5-dof-37000k.xml" --type "$TYPE" --newton_tol "$TOL" --krylovnonzero "$KRYLNONZERO"
+bsub -W 24:00 -n 1 mpirun ./Performance-FisherSolver --name "weakscaling-600k" --meshname "lh-white-hull-flood-0-1-merge-5-dof-600k.xml" --type "$TYPE" --newton_tol "$TOL" --krylovnonzero "$KRYLNONZERO"
+bsub -W 24:00 -n 8 mpirun ./Performance-FisherSolver --name "weakscaling-600k" --meshname "lh-white-hull-flood-0-1-merge-5-dof-4700k.xml" --type "$TYPE" --newton_tol "$TOL" --krylovnonzero "$KRYLNONZERO"
+bsub -W 24:00 -n 60 mpirun ./Performance-FisherSolver --name "weakscaling-600k" --meshname "lh-white-hull-flood-0-1-merge-5-dof-37000k.xml" --type "$TYPE" --newton_tol "$TOL" --krylovnonzero "$KRYLNONZERO"
