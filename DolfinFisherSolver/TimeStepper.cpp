97a9678a0eea765a1c454e7d07c317c7611a8ab5
sega surfs linux <piuskri@gmail.com>
2020-06-10 16:00:53 +0200

    max dt factor 1.1

--- DolfinFisherSolver/TimeStepper.cpp
+++ DolfinFisherSolver/TimeStepper.cpp
@@ -178,8 +178,8 @@ void TimeStepper::adaptiveTimestepping(int verbose, double frameDuration, std::s
 		// update timestep
 		double fac = adaptSafety_ * pow((adaptTol_ / nabla), (1/p));
 		// cap the maximum increase
-		if(fac > 1.2){
-			fac = 1.2;
+		if(fac > 1.1){
+			fac = 1.1;
 		}
 		dtNew = fac * dt;
 
