91593bfe643e33f79de37bb5e92574074b5086e5
sega surfs linux <piuskri@gmail.com>
2020-06-18 21:47:31 +0200

    change approach

--- mesh/analyze-mesh.py
+++ mesh/analyze-mesh.py
@@ -26,17 +26,18 @@ print("Analyze mesh...")
 V = FunctionSpace(mesh, "Lagrange", 2)
 
 dof_coords = V.tabulate_dof_coordinates()
+unchecked_coords = V.tabulate_dof_coordinates()
 min_dist = 99999
 max_dist = -99999
 counter = 1
 max_counter = len(dof_coords)
 for dof in dof_coords:
+    unchecked_coords = [x for x in unchecked_coords if (x[0] != dof[0] or x[1] != dof[1] or x[2] != dof[2])]
     counter = counter + 1
-    norms = np.linalg.norm(dof_coords - np.array(dof), axis=1)
-    norms = [x for x in dof_coords if x != 0]
-    min_dist = min(min_dist, np.min(norms))
+    worker = np.min(np.linalg.norm(unchecked_coords - np.array(dof), axis=1))
+    min_dist = min(min_dist, worker)
     print("{} ({}%)".format(min_dist, counter/max_counter * 100))
-    print("\t{} for pairs of {}".format(dof, np.min(worker_dist)))
+    print("\t{} for pairs of {}".format(dof, worker))
 
 print("Mesh Statistics of {}".format(mesh_name))
 print("\tDofs: ", max_counter)
