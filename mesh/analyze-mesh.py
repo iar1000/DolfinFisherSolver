804a92078ffcfa550d7c4a51f192e41695a7185a
sega surfs linux <piuskri@gmail.com>
2020-06-20 14:31:29 +0200

    subdivide into small areas to get approximation

--- mesh/analyze-mesh.py
+++ mesh/analyze-mesh.py
@@ -29,20 +29,24 @@ dof_coords = V.tabulate_dof_coordinates()
 unchecked_coords = V.tabulate_dof_coordinates()
 min_dist = 99999
 max_dist = -99999
-counter = 1
-max_counter = len(dof_coords)
 print("\tTotal nodes {}".format(len(dof_coords)))
 # remove all nodes which are not in the high resolution part of the mesh
-unchecked_coords = [x for x in unchecked_coords if sqrt((x[0]-46)**2 + (x[1]-132)**2 + (x[2]-67)**2) < 9]
+unchecked_coords_high = [x for x in unchecked_coords if sqrt((x[0]-46)**2 + (x[1]-132)**2 + (x[2]-67)**2) < 9]
 print("\tCompare {} nodes...".format(len(unchecked_coords)))
-# compare each dof to all others but itself (very slow) to find closest dofs
-for dof in unchecked_coords:
-    unchecked_coords = [x for x in unchecked_coords if (x[0] != dof[0] or x[1] != dof[1] or x[2] != dof[2])]
-    counter = counter + 1
-    worker = np.min(np.linalg.norm(unchecked_coords - np.array(dof), axis=1))
-    min_dist = min(min_dist, worker)
-    print("{} ({}%)".format(min_dist, counter/max_counter * 100))
-    print("\t{} for pairs of {}".format(dof, worker))
+subs = 1
+for subarea in [[46, 132, 67], [37, 130, 67], [51, 135, 67], [46, 132, 72], [46, 128, 60]]:
+    sub = [x for x in unchecked_coords_high if sqrt((x[0] - subarea[0]) ** 2 + (x[1] - subarea[1]) ** 2 + (x[2] - subarea[2]) ** 2) < 3]
+    counter = 1
+    max_counter = len(sub)
+    # compare each dof to all others but itself (very slow) to find closest dofs
+    for dof in sub:
+        sub = [x for x in sub if (x[0] != dof[0] or x[1] != dof[1] or x[2] != dof[2])]
+        counter = counter + 1
+        worker = np.min(np.linalg.norm(sub - np.array(dof), axis=1))
+        min_dist = min(min_dist, worker)
+        print("{} @ ({}%)".format(min_dist, counter/max_counter * 100))
+        print("\tcurrently round {}/{}, point {} @ {}".format(subs, len(subarea), dof, worker))
+    subs = subs + 1
 
 print("Mesh Statistics of {}".format(mesh_name))
 print("\tDofs: ", max_counter)
